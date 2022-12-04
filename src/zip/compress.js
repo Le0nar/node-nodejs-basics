b484e1a60d0b437703304fe814885b3c75c9e319
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2022-12-04 21:40:07 +0400

    feat: implement zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { unlink } from "fs/promises";
+
 const compress = async () => {
-    // Write your code here 
+    const readable = createReadStream('./src/zip/files/fileToCompress.txt')
+    const writable = createWriteStream('./src/zip/files/archive.gz')
+    const gzip = createGzip()
+
+    pipeline(readable, gzip, writable).then(() => unlink('./src/zip/files/fileToCompress.txt'))
 };
 
 await compress();
