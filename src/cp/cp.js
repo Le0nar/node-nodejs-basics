60b8cb149176aa09d43d7041565f1c72dc00b4c0
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2022-12-05 15:16:13 +0400

    feat: implement child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,23 @@
+import { spawn, } from "node:child_process";
+import { argv, stdin } from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const path = 'src/cp/files/script.js';
+
+    const child = spawn(`node`, [path, ...args], {
+        stdio: [stdin],
+    });
+
+    child.stdout.on('data', (data) => {
+        console.log(`Received from child process:\n${data}`);
+    });
+
+    child.on('spawn', () => {
+        console.log('\nSpawn child process!\n');
+    });
+
 };
 
-spawnChildProcess();
+const args = argv.slice(2);
+
+spawnChildProcess(args);
